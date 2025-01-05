2601974f799be65417b12dd0f0df4f9ec64071b7
Thorminate <thorwaag11@gmail.com>
2025-01-05 21:54:23 +0100

    Added compatibility to windows and refined the build script.

--- scripts/build.js
+++ scripts/build.js
@@ -1,17 +1,25 @@
 const logger = require("@clack/prompts");
 const color = require("picocolors");
 const { spawn } = require("child_process");
-const { renameSync, writeFileSync, mkdirSync, rmSync } = require("fs");
+const {
+  writeFileSync,
+  promises: { rm, mkdir, rename },
+} = require("fs");
 const configFile = require("../package.json");
 
 const cache = new Map();
 const version = configFile.version;
 const linuxFilename = `${configFile.build.productName} ${version} Linux-arm64`;
+const linuxFilenameX64 = `${configFile.build.productName} ${version} Linux-x64`;
 const macFilename = `${configFile.build.productName} ${version} Mac`;
 const winFilename = `${configFile.build.productName} ${version} Windows`;
 
 (async () => {
-  logger.intro(`Welcome to the build script ${color.blueBright("CLI")}`);
+  logger.intro(
+    `${color.blueBright("Build script")} for ${color.magenta(
+      `Obsidian Client`
+    )} ${color.greenBright(`v${version}`)}`
+  );
 
   const targets = await logger.multiselect({
     message: "Select targets:",
@@ -36,28 +44,32 @@ const winFilename = `${configFile.build.productName} ${version} Windows`;
   preBuild();
 })();
 
-const preBuild = function () {
+const preBuild = async function () {
   const s = logger.spinner();
-  cleanupPre(s);
+  await cleanupPre(s);
 
   buildVite();
 };
 
 const buildVite = function () {
   const s = logger.spinner();
-  s.start(`Building ${color.magenta("vite")} app...`);
+  s.start(`Building ${color.magentaBright("vite")} app...`);
 
-  const viteProcess = spawn("vue-tsc", ["--noEmit", "&&", "vite", "build"], {
-    shell: true,
-    stdio: ["ignore", "ignore", "inherit"],
-  });
+  const viteProcess = spawn(
+    "npx",
+    ["vue-tsc", "--noEmit", "&&", "npx", "vite", "build"],
+    {
+      shell: true,
+      stdio: ["ignore", "ignore", "inherit"],
+    }
+  );
 
   viteProcess.on("close", (code) => {
     if (code !== 0) {
       logger.log.error("Vite build failed.");
       process.exit(1);
     } else {
-      s.stop(`${color.magenta("Vite")} app ${color.green("built")}.`);
+      s.stop(`${color.magentaBright("Vite")} app ${color.green("built")}.`);
       buildTypescript();
     }
   });
@@ -65,17 +77,17 @@ const buildVite = function () {
 
 const buildTypescript = function () {
   const s = logger.spinner();
-  s.start(`Compiling ${color.blue("typescript")}...`);
-  const tscProcess = spawn("tsc", {
+  s.start(`Compiling ${color.bgBlue("typescript")}...`);
+  const tscProcess = spawn("npx", ["tsc"], {
     shell: true,
     stdio: ["ignore", "ignore", "inherit"],
   });
-  tscProcess.on("close", async (code) => {
+  tscProcess.on("close", (code) => {
     if (code !== 0) {
       logger.log.error("Typescript build failed.");
       process.exit(1);
     } else {
-      s.stop(`${color.blue("Typescript")} files ${color.green("compiled")}.`);
+      s.stop(`${color.bgBlue("Typescript")} files ${color.green("compiled")}.`);
       buildWindowsInstaller();
     }
   });
@@ -96,7 +108,7 @@ const buildWindowsInstaller = function () {
     recursive: true,
   });
 
-  const ebProcess = spawn("electron-builder", ["--win", "--x64"], {
+  const ebProcess = spawn("npx", ["electron-builder", "--win", "--x64"], {
     shell: true,
     stdio: ["ignore", "ignore", "inherit"],
   });
@@ -120,26 +132,32 @@ const buildLinuxInstallerX64 = function () {
   if (!isTarget) {
     buildMacInstaller();
     return;
+  } else if (process.platform !== "linux") {
+    logger.log.error(
+      "Linux builds can only be built on Linux. (see https://github.com/electron-userland/electron-build-service/issues/9) Try building on WSL."
+    );
+    buildMacInstaller();
+    return;
   }
 
   const s = logger.spinner();
-  s.start(`Building ${color.red("Linux")} x64 installer...`);
+  //s.start(`Building ${color.red("Linux")} x64 installer...`);
 
   writeFileSync(`releases/${version}/linux/${linuxFilename}.AppImage`, "", {
     recursive: true,
   });
 
-  const ebProcess = spawn("electron-builder", ["--linux", "--x64"], {
+  const ebProcess = spawn("npx", ["electron-builder", "--linux", "--x64"], {
     shell: true,
-    stdio: ["ignore", "ignore", "inherit"],
+    stdio: ["ignore", "inherit", "inherit"],
   });
 
   ebProcess.on("close", (code) => {
     if (code !== 0) {
-      s.stop("Linux x64 installer build failed.", 1);
+      //s.stop("Linux x64 installer build failed.", 1);
       process.exit(1);
     } else {
-      s.stop(`${color.red("Linux")} x64 installer ${color.green("built")}.`);
+      //s.stop(`${color.red("Linux")} x64 installer ${color.green("built")}.`);
       buildLinuxInstallerArm64();
     }
   });
@@ -154,7 +172,7 @@ const buildLinuxInstallerArm64 = function () {
     recursive: true,
   });
 
-  const ebProcess = spawn("electron-builder", ["--linux", "--arm64"], {
+  const ebProcess = spawn("npx", ["electron-builder", "--linux", "--arm64"], {
     shell: true,
     stdio: ["ignore", "ignore", "inherit"],
   });
@@ -177,9 +195,7 @@ const buildMacInstaller = function () {
     postBuild();
     return;
   } else if (process.platform !== "darwin") {
-    logger.log.error(
-      "Mac installer incompatible with this platform. Mac builds can only be built on MacOS, Skipping..."
-    );
+    logger.log.error("Mac builds can only be built on MacOS.");
     postBuild();
     return;
   }
@@ -191,7 +207,7 @@ const buildMacInstaller = function () {
     recursive: true,
   });
 
-  const ebProcess = spawn("electron-builder", ["--mac", "--universal"], {
+  const ebProcess = spawn("npx", ["electron-builder", "--mac", "--universal"], {
     shell: true,
     stdio: ["ignore", "ignore", "inherit"],
   });
@@ -210,51 +226,108 @@ const postBuild = function () {
   const s = logger.spinner();
   s.start("Build(s) complete, cleaning up...");
   cleanupPost();
-  s.stop("Cleanup complete.");
-  logger.log.success("Build(s) complete and ready for distribution.");
+  s.stop(`${color.cyanBright("Cleanup")} ${color.green("complete")}.`);
+  logger.outro(
+    `Build(s) ${color.green("complete")} and ${color.green(
+      "ready"
+    )} for ${color.magentaBright("distribution")}.`
+  );
 };
 
-const cleanupPre = function (s) {
-  s.start(
+const cleanupPre = async function (s) {
+  await s.start(
     `Removing interfering builds and structuring ${color.yellow(
       "directories"
     )}...`
   );
 
-  rmSync("dist", { recursive: true, force: true });
-  rmSync("releases/temp", { recursive: true, force: true });
-  rmSync(`releases/${version}`, {
-    recursive: true,
-    force: true,
+  await rm("dist", { recursive: true, force: true }).catch((err) => {
+    if (err.code === "EBUSY") {
+      logger.log.error(
+        "A file in the releases/temp directory is in use. Restart your computer and try again."
+      );
+    } else if (err.code === "ENOENT") {
+    } else {
+      logger.log.error(err);
+    }
   });
-  rmSync(`releases/${version}`, {
-    recursive: true,
-    force: true,
+  await rm("releases/temp", { recursive: true, force: true }).catch((err) => {
+    if (err.code === "EBUSY") {
+      logger.log.error(
+        "A file in the releases/temp directory is in use. Restart your computer and try again."
+      );
+    } else if (err.code === "ENOENT") {
+    } else {
+      logger.log.error(err);
+    }
   });
+  await rm(`releases/${version}`, { recursive: true, force: true }).catch(
+    (err) => {
+      if (err.code === "EBUSY") {
+        logger.log.error(
+          "A file in the releases/temp directory is in use. Restart your computer and try again."
+        );
+      } else if (err.code === "ENOENT") {
+      } else {
+        logger.log.error(err);
+      }
+    }
+  );
 
-  mkdirSync(`releases/${version}/win`, { recursive: true });
-  mkdirSync(`releases/${version}/mac`, { recursive: true });
-  mkdirSync(`releases/${version}/linux`, { recursive: true });
+  await mkdir(`releases/${version}/win`, { recursive: true });
+  await mkdir(`releases/${version}/mac`, { recursive: true });
+  await mkdir(`releases/${version}/linux`, { recursive: true });
 
-  s.stop(`Directories ${color.green("structured")}.`);
+  await s.stop(`Directories ${color.green("structured")}.`);
 };
 
-const cleanupPost = function () {
-  rmSync("dist", { recursive: true, force: true });
-  rmSync("releases/temp", { recursive: true, force: true });
-  rmSync(`releases/${version}/mac/${macFilename}.dmg.blockmap`, {
-    recursive: true,
-    force: true,
+const cleanupPost = async function () {
+  await rm("dist", { recursive: true, force: true }).catch((err) => {
+    if (err.code === "EBUSY") {
+      logger.log.error(
+        "A file in the releases/temp directory is in use. Restart your computer and try again."
+      );
+    }
+    if (err.code === "ENOENT") {
+    } else {
+      logger.log.error(err);
+    }
   });
-  rmSync(`releases/${version}/win/${winFilename}.exe.blockmap`, {
+  await rm("releases/temp", { recursive: true, force: true }).catch((err) => {
+    if (err.code === "EBUSY") {
+      logger.log.error(
+        "A file in the releases/temp directory is in use. Restart your computer and try again."
+      );
+    } else if (err.code === "ENOENT") {
+    } else {
+      logger.log.error(err);
+    }
+  });
+  await rm(`releases/${version}/mac/${macFilename}.dmg.blockmap`, {
     recursive: true,
     force: true,
+  }).catch((err) => {
+    if (err.code === "ENOENT") {
+    } else if (err.code === "EBUSY") {
+    } else {
+      logger.log.error(err);
+    }
+  });
+  await rm(`releases/${version}/win/${winFilename}.exe.blockmap`, {
+    recursive: true,
+    force: true,
+  }).catch((err) => {
+    if (err.code === "ENOENT") {
+    } else if (err.code === "EBUSY") {
+    } else {
+      logger.log.error(err);
+    }
   });
 };
 
-const hotswap = function () {
-  renameSync(
-    `releases/${configFile.version}/linux/${configFile.build.productName} ${configFile.version} Linux-arm64.AppImage`,
-    `releases/${configFile.version}/linux/${configFile.build.productName} ${configFile.version} Linux-x64.AppImage`
+const hotswap = async function () {
+  await rename(
+    `releases/${configFile.version}/linux/${linuxFilename}.AppImage`,
+    `releases/${configFile.version}/linux/${linuxFilenameX64}.AppImage`
   );
 };
